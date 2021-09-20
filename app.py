8af7888f0138c6def3c4fffbec4b09a752495b19
davisrao <davisrao1@gmail.com>
2021-09-20 14:39:35 -0700

    wrote patch /post route for adding / editing cupcakes

--- app.py
+++ app.py
@@ -26,3 +26,48 @@ def list_single_cupcake(cupcake_id):
     cupcake = Cupcake.query.get_or_404(cupcake_id)
     serialized = cupcake.serialize()
     return jsonify(cupcake=serialized)
+
+@app.post('/api/cupcakes')
+def create_a_cupcake():
+    """add cupcake and return JSON {"cupcake": {id, flavor, size, rating, image}}"""
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"] or None
+
+    # data = {k: (v if v else None) for k, v in request.json}
+    # {k: v or None for k, v in request.json}
+    # {k: (v if v else None) for k, v in request.json}
+
+    cupcake = Cupcake(flavor=flavor,size=size,rating=rating,image=image)
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    serialized = cupcake.serialize()
+
+    return (jsonify(cupcake=serialized),201)
+
+@app.patch('/api/cupcakes/<int:cupcake_id>')
+def update_cupcake_data(cupcake_id):
+    """edit cupcake details and return JSON of edited cupcake"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    # is there a more efficient way with loops to do?
+
+    if 'flavor' in request.json:
+        cupcake.flavor = request.json['flavor']
+    if 'size' in request.json:
+        cupcake.size = request.json['size']
+    if 'rating' in request.json:
+        cupcake.rating = request.json['rating']
+    if 'image' in request.json:
+        cupcake.image = request.json['image']
+
+    serialized = cupcake.serialize()
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return jsonify(cupcake=serialized)
